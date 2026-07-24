98f6e89ca2cc13ebc73d8398c32af166cdd29541
Diksha Taur <dikshataur10@gmail.com>
2026-07-24 12:49:13 +0530

    add responsiveness

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -7,68 +7,61 @@ function Navbar() {
       className="navbar navbar-expand-lg border-bottom sticky-top"
       style={{ backgroundColor: "#fff" }}
     >
-      {" "}
-      <div className="container py-2">
-        {" "}
+      <div className="container p-2">
         <Link className="navbar-brand" to="/">
-          {" "}
-          <img
-            src="/assets/logo.svg"
-            alt="FinPulse Logo"
-            className="navbar-logo"
-          />{" "}
+          <img src="assets/logo.svg" alt="Logo" style={{ width: "25%" }} />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarMenu"
-          aria-controls="navbarMenu"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {/* Navigation Links */}
-        <div className="collapse navbar-collapse" id="navbarMenu">
-          <ul className="navbar-nav ms-auto align-items-lg-center gap-lg-4">
+
+        <form className="d-flex" role="search">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row gap-5">
             <li className="nav-item">
-              <Link className="nav-link" to="/signup">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/signup"
+              >
                 Signup
               </Link>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link active" aria-current="page" to="/about">
                 About
               </Link>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link" to="/product">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/product"
+              >
                 Products
               </Link>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link" to="/pricing">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/pricing"
+              >
                 Pricing
               </Link>
             </li>
-
             <li className="nav-item">
-              <Link className="nav-link" to="/support">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/support"
+              >
                 Support
               </Link>
             </li>
-
-            <li className="nav-item">
-              <Link to="/login" className="btn btn-outline-dark login-btn">
+            <li className="nav-item pt-2 mx-2">
+              <Link to="/login" className="text-dark text-decoration-none">
                 Login
               </Link>
             </li>
           </ul>
-        </div>
+        </form>
       </div>
     </nav>
   );
