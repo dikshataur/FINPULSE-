1f051c6837793a736a16233289040160261195ff
Diksha Taur <dikshataur10@gmail.com>
2026-07-24 11:09:11 +0530

    fixed session authentication

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function OpenAccount() {
+  const navigate = useNavigate();
+  
   return (
     <div className="container p-5 mb-5">
       <div className="row text-center">
